938f6d7cbd135ec39127688994b535e38d3865a3
tjlane <thomas.joseph.lane@gmail.com>
2026-05-25 16:40:04 -0400

    prep for pypi

--- src/stardust/__init__.py
+++ src/stardust/__init__.py
@@ -3,7 +3,10 @@
 from stardust.refinementlogger.config import RefinementConfig
 from stardust.refinementlogger.engine import RefinementEngine
 
-__version__ = "0.1.0"
+try:
+    from stardust._version import version as __version__
+except ImportError:  # pragma: no cover - fallback when not installed
+    __version__ = "0.0.0"
 
 __all__ = [
     "RefinementEngine",
